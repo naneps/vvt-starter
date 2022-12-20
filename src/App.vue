8baa4fcc3b35b81c48c39b006a38950a4de560db
Nandang Eka Prasetya <ekaprasetya2244@gmail.com>
2022-12-20 12:27:42 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <main class="h-screen w-full bg-teal-300">
+    <div class="flex flex-col justify-center h-screen items-center">
+      <h1 class="text-2xl font-bold">Vite Vue Tailwindcss Starter</h1>
+      <button
+        class="
+          bg-white
+          p-3
+          rounded-md
+          my-2
+          hover:bg-slate-100
+          transition-all
+          ease-in-out
+          duration-300
+          font-semibold
+        "
+      >
+        Clone It
+      </button>
+    </div>
+  </main>
+</template>
+<script>
+export default {
+  methods: {
+    cloneIt() {},
+  },
+};
+</script>
+<style>
+</style>
