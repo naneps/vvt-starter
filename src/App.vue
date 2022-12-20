cb01bb8b28fa624645437fb53a54b73e86f0cdf4
Nandang Eka Prasetya <ekaprasetya2244@gmail.com>
2022-12-20 12:59:53 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
-<template>
-  <main class="h-screen w-full bg-teal-300">
+  <template>
+  <main class="h-screen w-full bg-slate-800">
     <div class="flex flex-col justify-center h-screen items-center">
-      <h1 class="text-2xl font-bold">Vite Vue Tailwindcss Starter</h1>
+      <h1 class="text-4xl font-bold text-white text-animation">
+        Vite Vue Tailwindcss Starter
+      </h1>
       <button
         class="
           bg-white
-          p-3
+          px-5
+          py-2
           rounded-md
           my-2
           hover:bg-slate-100
@@ -14,18 +17,54 @@
           duration-300
           font-semibold
         "
+        @click="cloneIt"
       >
-        Clone It
+        Use Template
       </button>
     </div>
   </main>
 </template>
 <script>
 export default {
+  computed: {},
   methods: {
-    cloneIt() {},
+    cloneIt() {
+      window.open("https://github.com/naneps/vvt-starter/");
+    },
   },
 };
 </script>
 <style>
+.text-animation {
+  /* make animation change random color */
+  animation: text-animation 1s infinite;
+  transition: all 0.5s ease-in-out;
+}
+@keyframes text-animation {
+  0% {
+    color: #f56565;
+    transition: all 0.5s ease-in-out;
+    /* transform: rotate(2deg); */
+  }
+  25% {
+    color: #d1fe82;
+    transform: rotate(4deg);
+    /* transition: all 0.5s ease-in-out; */
+  }
+  50% {
+    color: #48bb78;
+    /* transition: all 0.5s ease-in-out; */
+    transform: rotate(0deg);
+  }
+  75% {
+    color: #4299e1;
+    transform: rotate(-4deg);
+    /* transition: all 0.5s ease-in-out; */
+  }
+  100% {
+    /* transition: all 0.5s ease-in-out; */
+    color: #f56565;
+    transform: rotate(-2deg);
+  }
+}
 </style>
